cdb84ab59943b73a802cfe148ae44235297da962
AsterinGray <raymond.tiandinatan@gdn-commerce.com>
2023-05-26 10:57:38 +0700

    feat(navbar): improve navbar responsive

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -1,26 +1,55 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./styles.module.scss";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 const Navbar: React.FC = () => {
-  return (
-    <header className={styles.header}>
-      <Link href={"/"}>
-        <Image
-          src="/indonesia.svg"
-          alt="Indonesia Flag"
-          width={80}
-          height={30}
-        />
-      </Link>
+  const isMobile = useMediaQuery(`(max-width: 768px)`);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const renderNav = () => {
+    return (
       <nav className={styles.nav}>
         <Link href={"/provinsi"}>Provinsi</Link>
         <Link href={"/kota"}>Kota</Link>
         <Link href={"/kecamatan"}>Kecamatan</Link>
       </nav>
-    </header>
+    );
+  };
+
+  const renderMenu = () => {
+    return <div className={styles.menu}>{renderNav()}</div>;
+  };
+
+  const toogleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
+  return (
+    <>
+      <header className={styles.header}>
+        <Link href={"/"}>
+          <Image
+            src="/indonesia.svg"
+            alt="Indonesia Flag"
+            width={80}
+            height={30}
+          />
+        </Link>
+        {isMobile ? (
+          <nav className={styles.nav} onClick={toogleMenu}>
+            Menu
+          </nav>
+        ) : (
+          renderNav()
+        )}
+      </header>
+      {isMenuVisible && renderMenu()}
+    </>
   );
 };
 
